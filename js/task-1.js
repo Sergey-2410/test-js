dc354fde2f313f70f8392567d8a300c2d9d2cc45
Sergey-2410 <167428697+Sergey-2410@users.noreply.github.com>
2024-08-08 11:40:05 +0300

    add

---
+++ js/task-1.js
@@ -0,0 +1,40 @@
+// Якщо змінна a дорівнює 10, виведіть 'Вірно', інакше виведіть 'Невірно'.
+// function checkNumber(a) {
+//   if (a === 10) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+// console.log(checkNumber(5));
+
+//Змінна num може набувати 4 значення: 1, 2, 3 або 4. Якщо вона має
+// значення '1', то у змінну result запишемо 'зима', якщо має значення
+// '2' - 'весна' і так далі.
+// function checkSeason(num) {
+//   let result = "";
+//   switch (num) {
+//     case 1:
+//       result "зима";
+//       break;
+//     case 2:
+//       result "весна";
+//       break;
+//     case 3:
+//       result "літо";
+//       break;
+//     case 4:
+//       result "осінь";
+//           break;
+//       default:result = "Не вірно"
+//     }
+//     return result;
+// }
+// console.log(checkSeason(2));
+
+// Напишіть програму, яка отримає від користувача
+//число (кількість хвилин) і виведе у консоль
+//рядок у форматі годин і хвилин
+//70 === 01:10
+
+function formatTime() {}
